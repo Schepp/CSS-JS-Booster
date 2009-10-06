621a68239871ad22186f15f5bbcea5da37b87959
Christian Schepp Schaefer <schaepp@gmx.de>
2009-10-06 10:41:48 +0200

    Changed font to replace
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- example/example1/js/z_functions.js
+++ example/example1/js/z_functions.js
@@ -2,9 +2,9 @@ window.onload = function(){
 	milliseconds = (new Date()).getTime() - beforeload;
 	alert((milliseconds / 1000) + ' seconds load time');
 }
-Cufon.replace('h2', { fontFamily: 'Aller' });
-Cufon.replace('h3', { fontFamily: 'Aller' });
-Cufon.replace('p.greentext', { fontFamily: 'Aller' });
-Cufon.replace('p.services', { fontFamily: 'Aller' });
-Cufon.replace('p.browntext', { fontFamily: 'Notethis' });
+Cufon.replace('h2', { fontFamily: 'Sansation' });
+Cufon.replace('h3', { fontFamily: 'Sansation' });
+Cufon.replace('p.greentext', { fontFamily: 'Sansationr' });
+Cufon.replace('p.services', { fontFamily: 'Sansation' });
+Cufon.replace('p.browntext', { fontFamily: 'Sansation' });
 Cufon.now();
