73ce89cf5c7c7c2fb9893d5cfdd95ecd0133e2e1
Christian Schepp Schaefer <schaepp@gmx.de>
2009-10-08 16:36:42 +0200

    fixed a bug in path-cleaning-regex
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- booster/booster_mhtml.php
+++ booster/booster_mhtml.php
@@ -27,9 +27,10 @@ header("Content-type: text/plain");
 
 include('booster_inc.php');
 
-((isset($_GET['dir'])) ? $dir = rtrim(preg_replace('/[^a-zA-Z\.\/]/','',$_GET['dir']),'/') : $dir = 'css');
-$mhtmlfile = 'booster_cache/'.preg_replace('/[^a-z]/i','',$dir).'_datauri_mhtml_cache.txt';
+((isset($_GET['dir'])) ? $dir = rtrim(preg_replace('/[^a-zA-Z0-9,\.\/]/','',$_GET['dir']),'/') : $dir = 'css');
+$mhtmlfile = 'booster_cache/'.preg_replace('/[^a-z0-9]/i','',$dir).'_datauri_mhtml_cache.txt';
 
 if(!file_exists($mhtmlfile)) booster_css($dir);
 if(file_exists($mhtmlfile)) echo file_get_contents($mhtmlfile);
+else echo $mhtmlfile;
 ?>
