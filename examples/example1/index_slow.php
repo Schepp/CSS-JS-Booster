36de39e63b7948b093fa4fc796fa4aec969c6d6a
Christian Schepp Schaefer <schaepp@gmx.de>
2009-10-11 20:18:13 +0200

    Reordered and regrouped examples and testcases
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

---
+++ examples/example1/index_slow.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Blog Division</title>
+<script type="text/javascript">beforeload = (new Date()).getTime();</script>
+<link rel="stylesheet" media="all" title="Standard" type="text/css" href="css/style.css" />
+</head>
+<body>
+<div id="topPan">
+  <div id="topHeaderPan">
+    <ul>
+      <li class="company">Company</li>
+      <li><a href="http://www.free-css.com/">Solutions</a></li>
+      <li><a href="http://www.free-css.com/">Partners</a></li>
+      <li class="client"><a href="http://www.free-css.com/">Client </a></li>
+    </ul>
+    <a href="http://www.free-css.com/"><img src="images/logo.jpg" title="Blog Division" alt="Blog Division" width="191" height="84" border="0" /></a> </div>
+  <div id="toprightPan">
+    <ul>
+      <li class="home">Home</li>
+      <li class="about"><a href="http://www.free-css.com/">About us</a></li>
+      <li class="contact"><a href="http://www.free-css.com/">contact</a></li>
+    </ul>
+  </div>
+</div>
+<div id="bodyPan">
+  <div id="bodyleftPan">
+    <h2>Coordination</h2>
+    <p class="greentext">M Robert</p>
+    <p class="browntext">23rd september 2006</p>
+    <p>Blog Division is a free, tableless, W3C-compliant web design layout by Template World. This template has been tested and proven compatible with all major browser environments and operating systems. You are free to modify the design to suit your tastes in any way you like.</p>
+    <p>We only ask you to not remove "Design by Template World" and the link http://www.templateworld.com from the footer of the template.</p>
+    <p>If you are interested in seeing more of our free web template designs feel free to visit our website, Template World. We intend to add at least 25 new free templates in the coming month.</p>
+    <ul>
+      <li class="more"><a href="http://www.free-css.com/">Read more</a></li>
+      <li class="comment">Comments: 03</li>
+    </ul>
+    <h3>Destination</h3>
+    <p class="greentext">L Piterson</p>
+    <p class="browntext">03rd october 2006</p>
+    <p>Maecenas cursus risus et orci. In sit amet purus ac mauris egestas pharetra. Vivamus nibh. Vestibulum congue. Maecenas nunc. In hac habMitasse platea dictumst. PraesentMaecenas aucto Maecenas odio in ante id risus.sapien. Quisque lacus. Phasellus f </p>
+    <p>ermentum ante id risus. Surispendisse tempuss. Maecenas dolor puruspusdted, sodales tedaseccumsan sed, congue at, loremcongue sedapsucong enas cursus </p>
+    <ul>
+      <li class="more"><a href="http://www.free-css.com/">Read more</a></li>
+      <li class="comment">Comments: 03</li>
+    </ul>
+  </div>
+  <div id="bodyrightPan">
+    <div id="loginPan">
+      <h2>members <span>login</span></h2>
+      <form action="http://www.free-css.com/" method="post">
+        <label>Email Id</label>
+        <input name="email" type="text" id="email" />
+        <label>Password</label>
+        <input name="pass" type="password" id="pass" />
+        <input name="Input" type="submit" class="button" value="Login" />
+      </form>
+      <ul>
+        <li class="nonregister">Not a member &nbsp;?</li>
+        <li class="register"><a href="http://www.free-css.com/">Resister now</a> </li>
+      </ul>
+    </div>
+    <div id="loginBottomPan">&nbsp;</div>
+    <p class="hours"><a href="http://www.free-css.com/">24/7 hours</a></p>
+    <div id="servicesPan">&nbsp;</div>
+    <div id="servicesBodyPan">
+      <p class="services">Services</p>
+      <p class="boldbrowntext">Morbi sodales ante a mauris. Vestibuc netmomaurisle </p>
+      <p>quamsectetuer mauris et quam.odio loremsto, molestie ut, laoreet vitae, luctus vitae, nisi. Cur abitur nibh justo, tincidunt nec, tempor eget te, tincidunt amet, libero</p>
+      <p class="bluetext">&quot;risus et orci. In sit amet purus ac maurisege egestas pharetra. Vivamus nibh. Vestibulum congue.&quot;</p>
+      <p class="largebrowntext">tincidunt amet libero</p>
+      <p class="link"><a href="http://www.free-css.com/">click here to know more</a></p>
+    </div>
+    <div id="servicesBottomPan">&nbsp;</div>
+  </div>
+</div>
+<div id="footermainPan">
+  <div id="footerPan">
+    <div id="footerlogoPan"><a href="http://www.free-css.com/"><img src="images/footerlogo.gif" title="Blog Division" alt="Blog Division" width="189" height="87" border="0" /></a></div>
+    <ul>
+      <li><a href="http://www.free-css.com/">Home</a>| </li>
+      <li><a href="http://www.free-css.com/">About Us</a> | </li>
+      <li><a href="http://www.free-css.com/">Company</a>| </li>
+      <li><a href="http://www.free-css.com/">Solutions</a> | </li>
+      <li><a href="http://www.free-css.com/">Partners</a> |</li>
+      <li><a href="http://www.free-css.com/">Client</a> |</li>
+      <li><a href="http://www.free-css.com/">Contact</a> </li>
+    </ul>
+    <p class="copyright">© 9blog division. All rights reserved.</p>
+    <ul class="templateworld">
+      <li>design by:</li>
+      <li><a href="http://www.templateworld.com" target="_blank">Template World</a></li>
+    </ul>
+    <ul class="validation">
+      <li>Valid:</li>
+      <li><a href="http://validator.w3.org/check?uri=referer" target="_blank">XHTML</a></li>
+      <li> Valid:</li>
+      <li><a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">CSS</a></li>
+    </ul>
+  </div>
+</div>
+<script type="text/javascript" src="js/01_jquery-1.3.1.min.js"></script>
+<script type="text/javascript" src="js/02_cufon-yui.js"></script>
+<script type="text/javascript" src="js/03_Sansation_400.font.js"></script>
+<script type="text/javascript" src="js/z_functions.js"></script>
+</body>
+</html>
