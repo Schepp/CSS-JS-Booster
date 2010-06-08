e3f37f798c9457b5ec29ee0a28e71c7a35ed9e54
Christian Schepp Schaefer <schaepp@gmx.de>
2010-06-08 18:57:10 +0800

    Added functionality to detect a mod_rewritten caller-file so that the relative path to the Booster-markup gets adapted accordingly.
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- booster/booster_inc.php
+++ booster/booster_inc.php
@@ -982,7 +982,7 @@ class Booster {
 		$markup = '';
 		
 		// Calculate possible relative path-offset created by mod_rewrite
-		$booster_offset_path = rtrim($this->getpath(dirname($_SERVER['REQUEST_URI']),dirname($_SERVER['SCRIPT_NAME'])),'/');
+		$booster_offset_path = rtrim($this->getpath((substr($_SERVER['REQUEST_URI'],strlen($_SERVER['REQUEST_URI']) - 1,1) == '/' ? $_SERVER['REQUEST_URI'] : dirname($_SERVER['REQUEST_URI'])),dirname($_SERVER['SCRIPT_NAME'])),'/');
 		// Calculate relative path from calling script to booster-folder
 		$booster_path = $booster_offset_path.'/'.$this->getpath(str_replace('\\','/',dirname(__FILE__)),dirname($_SERVER['SCRIPT_FILENAME']));
 		// Calculate relative path from booster-folder to calling script
@@ -1188,7 +1188,7 @@ class Booster {
 		$markup = '';
 
 		// Calculate possible relative path-offset created by mod_rewrite
-		$booster_offset_path = rtrim($this->getpath(dirname($_SERVER['REQUEST_URI']),dirname($_SERVER['SCRIPT_NAME'])),'/');
+		$booster_offset_path = rtrim($this->getpath((substr($_SERVER['REQUEST_URI'],strlen($_SERVER['REQUEST_URI']) - 1,1) == '/' ? $_SERVER['REQUEST_URI'] : dirname($_SERVER['REQUEST_URI'])),dirname($_SERVER['SCRIPT_NAME'])),'/');
 		// Calculate relative path from calling script to booster-folder
 		$booster_path = $booster_offset_path.'/'.$this->getpath(str_replace('\\','/',dirname(__FILE__)),dirname($_SERVER['SCRIPT_FILENAME']));
 		// Calculate relative path from booster-folder to calling script
