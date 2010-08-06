af4c0ff3a638bae54a0423233e5c2299efe6a2ca
Christian Schepp Schaefer <schaepp@gmx.de>
2010-08-06 11:26:37 +0200

    Did even further code re-beautification.
    Signed-off-by: Christian Schepp Schaefer <schaepp@gmx.de>

--- booster/booster_inc.php
+++ booster/booster_inc.php
@@ -1,34 +1,34 @@
 <?php
 
 /**
- * CSS-JS-BOOSTER
- * 
- * An easy to use PHP-Library that combines, optimizes, dataURI-fies, re-splits, 
- * compresses and caches your CSS and JS for quicker loading times.
- * 
- * PHP version 5
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published 
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.
- * If not, see <http://www.gnu.org/licenses/lgpl-3.0.txt>
- * 
- * @category  PHP 
- * @package   CSS-JS-Booster 
- * @author    Christian Schepp Schaefer <schaepp@gmx.de> <http://twitter.com/derSchepp>
- * @copyright 2010 Christian Schepp Schaefer
- * @license   http://www.gnu.org/copyleft/lesser.html The GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0
- * @link      http://github.com/Schepp/CSS-JS-Booster 
- */
+* CSS-JS-BOOSTER
+* 
+* An easy to use PHP-Library that combines, optimizes, dataURI-fies, re-splits, 
+* compresses and caches your CSS and JS for quicker loading times.
+* 
+* PHP version 5
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published 
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.
+* If not, see <http://www.gnu.org/licenses/lgpl-3.0.txt>
+* 
+* @category  PHP 
+* @package   CSS-JS-Booster 
+* @author    Christian Schepp Schaefer <schaepp@gmx.de> <http://twitter.com/derSchepp>
+* @copyright 2010 Christian Schepp Schaefer
+* @license   http://www.gnu.org/copyleft/lesser.html The GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0
+* @link      http://github.com/Schepp/CSS-JS-Booster 
+*/
 
 // Starting zlib-compressed output
 @ini_set('zlib.output_compression',2048);
@@ -46,23 +46,23 @@ else @ob_start();
 
 
 /**
- * Inclusion of user agent detection class
- */
+* Inclusion of user agent detection class
+*/
 include_once('browser_class_inc.php');
 
 /**
- * CSS-JS-BOOSTER
- * 
- * An easy to use PHP-Library that combines, optimizes, dataURI-fies, re-splits, 
- * compresses and caches your CSS and JS for quicker loading times.
- * 
- * @category  PHP 
- * @package   CSS-JS-Booster 
- * @author    Christian Schepp Schaefer <schaepp@gmx.de> <http://twitter.com/derSchepp>
- * @copyright 2009 Christian Schepp Schaefer
- * @license   http://www.gnu.org/copyleft/lesser.html The GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0
- * @link      http://github.com/Schepp/CSS-JS-Booster 
- */
+* CSS-JS-BOOSTER
+* 
+* An easy to use PHP-Library that combines, optimizes, dataURI-fies, re-splits, 
+* compresses and caches your CSS and JS for quicker loading times.
+* 
+* @category  PHP 
+* @package   CSS-JS-Booster 
+* @author    Christian Schepp Schaefer <schaepp@gmx.de> <http://twitter.com/derSchepp>
+* @copyright 2009 Christian Schepp Schaefer
+* @license   http://www.gnu.org/copyleft/lesser.html The GNU LESSER GENERAL PUBLIC LICENSE, Version 3.0
+* @link      http://github.com/Schepp/CSS-JS-Booster 
+*/
 class Booster {
 
 // Global configuration /////////////////////////////////////////////////////////////////////////////////////////////////////////////
